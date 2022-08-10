ea9dd08cf351422d4c73a3791606338bb81992ac
kim0100 <gyals7471@naver.com>
2022-08-10 10:51:50 +0900

    회사소개

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 // html dom 이 다 로딩된 후 실행된다.    
 $(document).ready(function(){
-    // menu 클래스 바로 하위에 있는 태그를 클릭했을때        
-    $(".hamburger-menu").click(function(){
-      let hidenave = $(".hide-nave");
-       let submenu = $(".hide-nav-texts");
-       if(hidenave.css("display") == "none"){
-         hidenave.slideDown();
-         submenu.slideDown();
-       }
-       else{
-         hidenave.slideUp();
-         submenu.slideUp();
-       }
-    });
- });
+   // menu 클래스 바로 하위에 있는 태그를 클릭했을때        
+   $(".hamburger-menu").click(function(){
+     let hidenave = $(".hide-nave");
+      let submenu = $(".hide-nav-texts");
+      if(hidenave.css("display") == "none"){
+        hidenave.slideDown();
+        submenu.slideDown();
+      }
+      else{
+        hidenave.slideUp();
+        submenu.slideUp();
+      }
+   });
+});
