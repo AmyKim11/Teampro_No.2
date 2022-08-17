b3bbf8327407c10d5f87bda3f91ee4ec2535ac23
ossi0077 <ossi0077@naver.com>
2022-08-17 17:55:05 +0900

    update slider drag function

--- js/slider.js
+++ js/slider.js
@@ -1,24 +1,32 @@
-$('.carousel').carousel({
-    interval : 3000,
-    // touch : true
-});
-
 let slide_cont_status = 0;
-const slider = $('#slider_item');
+const slider = $('.carousel_inner');
 const switch_button_time = 100;
 const img_path = "./img/slider/"
 const pause_button = $('.pauseplay_img');
 
-pause_button.click(switch_playpause);
+$(document).ready(function(){
+    slider.slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+        prevArrow: $('.preview_button'),
+        nextArrow: $('.next_button')
+    });
 
-$('.preview_button').click(switch_button_img);
+    pause_button.click(switch_playpause);
+    
+    $('.preview_button').click(switch_button_img);
 
-$('.next_button').click(switch_button_img);
+    $('.next_button').click(switch_button_img);
+
+})
+
+
 
 function switch_playpause(){
     
     if(slide_cont_status==0){
-        slider.carousel('pause');
+        slider.slick('slickPause')
         pause_button.fadeOut(switch_button_time, function(){
         pause_button.attr('src', img_path+'play.svg');
         pause_button.fadeIn(switch_button_time);
@@ -26,7 +34,7 @@ function switch_playpause(){
 
         slide_cont_status=1;
     }else if(slide_cont_status==1){
-        slider.carousel('cycle');
+        slider.slick('slickPlay')
         pause_button.fadeOut(switch_button_time, function(){
         pause_button.attr('src', img_path+'pause.svg');
         pause_button.fadeIn(switch_button_time);
@@ -38,7 +46,7 @@ function switch_playpause(){
 
 function switch_button_img(){
     if(slide_cont_status==1){
-        slider.carousel('cycle');
+        slider.slick('slickPlay')
         pause_button.fadeOut(switch_button_time, function(){
         pause_button.attr('src', img_path+'pause.svg');
         pause_button.fadeIn(switch_button_time);
